76be7c88b094b27ae66c3740faa8fe36f05937de
Jimmie43 <jamesmagi043@gmail.com>
2023-03-17 06:26:40 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 /**
 * main - A programme that uses puts function
@@ -7,11 +6,6 @@
 */
 int main(void)
 {
-puts('\*Programming is like building a multilingual puzzle');
+puts('\*Programming is like building a multilingual puzzle;);
 return (0);
 }
-
-=======
-#!/bin/bash
-int main() { puts("Programming is like building a multilingual puzzle\n"); return 0; }
->>>>>>> a576e2b04b1a6a4c6c8ba949ea1ac12c690ef5a1
