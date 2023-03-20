8204ed70f32ee9642901a1dfe12ae2b2cb0c69d8
Jimmie43 <jamesmagi043@gmail.com>
2023-03-20 13:00:43 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Description: 'excluding q and e'
+  *Return always 0
+  */
+int main(void)
+{
+	char low, e, q;
+	e = 'e';
+	q = 'q';
+		for (low = 'a'; low <= 'z'; low++)
+		{
+			if (low != e && low != q)
+				putchar(low);
+		}
+	putchar('\n');
+	return (0);
+}
