69e3511ea3a01437de5170bcd07d89a156aa710e
Jimmie43 <jamesmagi043@gmail.com>
2023-03-20 13:06:41 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,13 +2,17 @@
 /**
   *main - Entry point
   *Description: 'excluding q and e'
-  *Return always 0
+  *Return: always 0
   */
+
 int main(void)
+
 {
 	char low, e, q;
+
 	e = 'e';
 	q = 'q';
+
 		for (low = 'a'; low <= 'z'; low++)
 		{
 			if (low != e && low != q)
