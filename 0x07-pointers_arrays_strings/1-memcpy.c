bfb874e50531afe5394a31e68cd899536e0e10fb
Jimmie43 <jamesmagi043@gmail.com>
2023-04-04 11:58:17 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _memcpy - the entry point
+  * @dest: pointer to destination
+  * @src: pointer to the source
+  * @n: number of bytes to copy
+  */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int j;
+
+	for (j = 0; j < n; j++)
+	{
+		dest[j] = src[j];
+	}
+	return (dest);
+}
